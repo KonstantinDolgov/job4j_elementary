7399fc954c548a631ca15c250b578eb727869978
KonstantinDolgov <dolgovkos777@gmail.com>
2022-08-29 19:43:57 +0300

    4.2. Тернарное сравнение [#189 #337318]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -7,7 +7,6 @@ public class Max {
 
     public static int max(int left, int right) {
         boolean condition = left > right;
-        int result = condition ? left : right;
-        return result;
+        return condition ? left : right;
     }
 }
